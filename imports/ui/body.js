c3de208baa762c0c9dbf434b6098b5e81de38241
lucas persson <luben93@gmail.com>
2016-05-25 16:51:29 +0200

    started working on assaingmnet

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,5 +1,6 @@
 import { Template } from 'meteor/templating';
 import { Tasks } from '../api/tasks.js';
+import { HTTP } from 'meteor/http';
 import './task.js';
 import './body.html';
 
@@ -16,10 +17,21 @@ Template.body.events({
 
         const target = event.target;
         const text = target.text.value;
+        let file = target.file.value;
+        HTTP.call("GET", file, {params:{}},function(err,result){
+            if(!err){
+                console.log("GETted");
+                result.toString.split('#').foreach(function(row){
+                    console.log("row: "+row)
+                })
+            }
+        });
+
 
         Tasks.insert({
             text,
             createdAt: new Date(),
+            file,
         });
     },
 });
